69b5970f5f1d7d796b062b2bd30a0a397918a5ec
mafia <mafia77f.business@gmail.com>
2026-01-05 08:25:20 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.leveluplife.app',
-  appName: 'Level Up Life',
+  appId: 'com.highest.setvoid',
+  appName: 'setvoid',
   webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  },
   plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#000000',
-      showSpinner: false
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#2d8a6e',
+      sound: 'beep.wav'
     }
   }
 };
