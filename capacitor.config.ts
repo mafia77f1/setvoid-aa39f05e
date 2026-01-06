5962cf7ef1059d59ff285fcab92cff301d065ac7
mafia <mafia77f.business@gmail.com>
2026-01-06 09:58:53 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.leveluplife.app',
-  appName: 'Level Up Life',
+  appId: 'com.setvoid.app',
+  appName: 'SETVOID',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
